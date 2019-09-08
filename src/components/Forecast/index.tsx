604a36f8c5a1bbfeca243bbf24384ea2d53a3fb7
embalex <a.zakharov.job@gmail.com>
2019-09-08 22:50:15 +0300

    Add width dependency for forecast estimate blocks. Update forecast mocks.

--- src/components/Forecast/index.tsx
+++ src/components/Forecast/index.tsx
@@ -13,15 +13,42 @@ interface IProps {
 }
 
 export const Forecast: React.FC<IProps> = ({ estimated }) => {
-  const renderedEstimates = estimated.map(estimate => (
-    <Estimate key={estimate.time} {...estimate} />
-  ));
+  const estWrapperRef = React.useRef<HTMLDivElement>(null);
+  const [ estimateList, setEstimateList ] = React.useState<React.ReactElement[]>([]);
+
+  React.useEffect(() => {
+    const updateEstimateList = (): void => {
+      if (!estWrapperRef.current) {
+        setEstimateList([]);
+
+        return;
+      }
+
+      const { clientWidth } = estWrapperRef.current;
+      const estimatesNums = clientWidth / 170;  // Magic number ((( 170px is width of Estimate's wrapper.
+
+      const list = estimated.reduce((acc: React.ReactElement[], estimate, estimateNumber) => {
+        if ( estimateNumber >= estimatesNums) { return acc; }
+
+        acc.push(<Estimate key={estimate.time} {...estimate} />);
+
+        return acc;
+      }, []);
+
+      setEstimateList(list);
+    };
+    updateEstimateList();
+
+    window.addEventListener('resize', updateEstimateList);
+
+    return () => window.removeEventListener('resize', updateEstimateList);
+  }, [estimated]);
 
   return (
     <ForecastWrapper>
       <Header />
-      <ForecastEstimatesWrapper>
-        {renderedEstimates}
+      <ForecastEstimatesWrapper ref={estWrapperRef}>
+        {estimateList}
       </ForecastEstimatesWrapper>
     </ForecastWrapper>
   );
